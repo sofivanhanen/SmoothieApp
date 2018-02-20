97af59d4131d03be3a8ab0ca0dfb12a5c75be471
sofivanhanen <sofia.vanhanen0@gmail.com>
2018-02-20 15:16:54 +0200

    Add RaakaAine object, table and DAO

---
+++ src/main/java/com/mycompany/smoothieapp/RaakaAineDao.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.smoothieapp;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author sofvanh
+ */
+public class RaakaAineDao {
+    
+    private Database database;
+    
+    public RaakaAineDao(Database database) {
+        this.database = database;
+    }
+    
+    public RaakaAine findOne(Integer key) throws SQLException {
+        try (Connection conn = database.getConnection()) {
+            PreparedStatement stmt = conn.prepareStatement("SELECT id, nimi FROM RaakaAine WHERE id = ?");
+            stmt.setInt(1, key);
+
+            ResultSet result = stmt.executeQuery();
+            if (!result.next()) {
+                return null;
+            }
+
+            return new RaakaAine(result.getInt("id"), result.getString("nimi"));
+        }
+    }
+    
+    public RaakaAine findByName(String nimi) throws SQLException {
+        try (Connection conn = database.getConnection()) {
+            PreparedStatement stmt = conn.prepareStatement("SELECT id, nimi FROM RaakaAine WHERE nimi = ?");
+            stmt.setString(1, nimi);
+
+            ResultSet result = stmt.executeQuery();
+            if (!result.next()) {
+                return null;
+            }
+
+            return new RaakaAine(result.getInt("id"), result.getString("nimi"));
+        }
+    }
+    
+    public List<RaakaAine> findAll() throws SQLException {
+        List<RaakaAine> raakaAineet = new ArrayList<>();
+
+        try (Connection conn = database.getConnection();
+            ResultSet result = conn.prepareStatement("SELECT id, nimi FROM RaakaAine").executeQuery()) {
+
+            while (result.next()) {
+                raakaAineet.add(new RaakaAine(result.getInt("id"), result.getString("nimi")));
+            }
+        }
+
+        return raakaAineet;
+    }
+    
+    public RaakaAine saveOrUpdate(RaakaAine raakaAine) throws SQLException {
+        RaakaAine byName = findByName(raakaAine.getNimi());
+
+        if (byName != null) {
+            return byName;
+        } 
+
+        try (Connection conn = database.getConnection()) {
+            PreparedStatement stmt = conn.prepareStatement("INSERT INTO RaakaAine (nimi) VALUES (?)");
+            stmt.setString(1, raakaAine.getNimi());
+            stmt.executeUpdate();
+        }
+
+        return findByName(raakaAine.getNimi());
+    }
+    
+    public void delete(Integer key) throws SQLException {
+        try (Connection conn = database.getConnection()) {
+            PreparedStatement stmt = conn.prepareStatement("DELETE FROM RaakaAine WHERE id = ?");
+            stmt.setInt(1, key);
+            stmt.executeUpdate();
+        }
+    }
+    
+    public void deleteByName(String nimi) throws SQLException {
+        try (Connection conn = database.getConnection()) {
+            PreparedStatement stmt = conn.prepareStatement("DELETE FROM RaakaAine WHERE nimi = ?");
+            stmt.setString(1, nimi);
+            stmt.executeUpdate();
+        }
+    }
+    
+}
