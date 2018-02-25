7e92f0ae6ff2463c99df42f667b973941fd17a4f
otsha <32258613+otsha@users.noreply.github.com>
2018-02-25 14:01:03 +0200

    Add functionality for deleting and adding recipes to DB, add test data to DB

--- src/main/java/com/mycompany/smoothieapp/SmoothieApp.java
+++ src/main/java/com/mycompany/smoothieapp/SmoothieApp.java
@@ -26,31 +26,41 @@ public class SmoothieApp {
 
         // Get index page
         Spark.get("/", (req, res) -> {
+            AnnosDao smoothiedao = new AnnosDao(db);
 
             HashMap map = new HashMap<>();
+            map.put("smoothiet", smoothiedao.findAll());
+
             return new ModelAndView(map, "index");
         }, new ThymeleafTemplateEngine());
-        
+
         // Get smoothiet page
         Spark.get("/smoothiet", (req, res) -> {
-            // SmoothieDao smoothiedao = new SmoothieDao(db);
+            AnnosDao smoothiedao = new AnnosDao(db);
             RaakaAineDao RADao = new RaakaAineDao(db);
-            
+
             HashMap map = new HashMap<>();
-            // map.put("smoothiet", smoothiedao.findAll());
+            map.put("smoothiet", smoothiedao.findAll());
             map.put("raakaAineet", RADao.findAll());
-            
+
             return new ModelAndView(map, "smoothiet");
         }, new ThymeleafTemplateEngine());
-        
+
         // Post add new smoothie
         Spark.post("/smoothiet/uusi", (req, res) -> {
-            // SmoothieDao smoothiedao = new SmoothieDao(db);
-            // Smoothie uusi = new Smoothie(0, req.queryParams("smoothie");
-            // SmoothieDao.saveOrUpdate(uusi);
-            
-            System.out.println("DEBUG: vastaanotettiin: " + req.queryParams("smoothie"));
-            
+            AnnosDao smoothiedao = new AnnosDao(db);
+            Annos uusi = new Annos(0, req.queryParams("smoothie"));
+            smoothiedao.saveOrUpdate(uusi);
+
+            res.redirect("/smoothiet");
+            return 0;
+        });
+
+        // Post delete smoothie recipe
+        Spark.post("/smoothiet/:id/poista", (req, res) -> {
+            AnnosDao smoothiedao = new AnnosDao(db);
+            smoothiedao.delete(Integer.parseInt(req.params(":id")));
+
             res.redirect("/smoothiet");
             return 0;
         });
@@ -64,24 +74,22 @@ public class SmoothieApp {
 
             return new ModelAndView(map, "raakaaineet");
         }, new ThymeleafTemplateEngine());
-        
+
         // Post add new raaka-aine
         Spark.post("/raakaaineet/uusi", (req, res) -> {
             RaakaAineDao RADao = new RaakaAineDao(db);
             RaakaAine uusi = new RaakaAine(0, req.queryParams("raakaA"));
             RADao.saveOrUpdate(uusi);
-            
+
             res.redirect("/raakaaineet");
             return 0;
         });
-        
+
         // Post delete raaka-aine
-        
         Spark.post("raakaaineet/:id/poista", (req, res) -> {
-            //System.out.println("saatiin poistettava: " + req.params(":id"));
             RaakaAineDao RADao = new RaakaAineDao(db);
             RADao.delete(Integer.parseInt(req.params(":id")));
-            
+
             res.redirect("/raakaaineet");
             return 0;
         });
